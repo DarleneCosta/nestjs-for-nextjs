744766a733126369d70dbf70b8892b1451f13afe
DarleneCosta <darl_mel@hotmail.com>
2025-08-09 06:36:26 -0300

    Adiciona dependências @nestjs/config, class-transformer e class-validator ao projeto. Atualiza AppModule para incluir ConfigModule e configurações globais. Modifica UsersController para retornar uma string de teste ao invés de chamar o UsersService. Remove importação do UsersService no UsersController e limpa o UsersModule.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,17 @@ import { AppService } from './app.service';
 import { AuthsModule } from './auths/auths.module';
 import { PostsModule } from './posts/posts.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthsModule, PostsModule, UsersModule],
+  imports: [
+    AuthsModule,
+    PostsModule,
+    UsersModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
   exports: [],
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,10 @@
 import { Controller, Get, Param } from '@nestjs/common';
-import { UsersService } from './users.service';
 import { CustomParseIntPipe } from 'src/common/pipes/custom-parse-int-pipe';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
-
   @Get(':id')
   getUserById(@Param('id', CustomParseIntPipe) id: number) {
-    return this.usersService.getUserById(id);
+    return `teste ${typeof id} ${id}`;
   }
 }
