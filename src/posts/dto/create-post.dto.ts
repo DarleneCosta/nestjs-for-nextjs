bb96bab3e46cc65531d0236e051c8d5558206c28
DarleneCosta <darl_mel@hotmail.com>
2025-08-13 05:26:07 -0300

    Adiciona validações e propriedades ao DTO de criação de post, incluindo título, resumo, conteúdo e URL da imagem de capa, melhorando a integridade dos dados recebidos.

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,1 +1,26 @@
-export class CreatePostDto {}
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUrl,
+  Length,
+} from 'class-validator';
+
+export class CreatePostDto {
+  @IsString({ message: 'Título deve ser uma string' })
+  @IsNotEmpty({ message: 'Título é obrigatório' })
+  @Length(10, 150, { message: 'Título deve ter entre 10 e 150 caracteres' })
+  title: string;
+
+  @IsString({ message: 'Resumo deve ser uma string' })
+  @Length(10, 200, { message: 'Resumo deve ter entre 10 e 200 caracteres' })
+  excerpt: string;
+
+  @IsString({ message: 'Conteúdo deve ser uma string' })
+  @IsNotEmpty({ message: 'Conteúdo é obrigatório' })
+  content: string;
+
+  @IsUrl({ require_tld: false })
+  @IsOptional()
+  coverImageUrl: string;
+}
